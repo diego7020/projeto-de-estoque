bfe896a106d93ff34541fbb3b7ed7237a381d63b
Diego Militao de Souza <diegomilitaosouza@gmail.com>
2024-08-21 13:51:45 -0400

    atualização espaço antes do + no cp1 e cp2

--- src/visao/ClasseProdutoVisao.java
+++ src/visao/ClasseProdutoVisao.java
@@ -12,15 +12,15 @@ public class ClasseProdutoVisao {
         cp1.setDataDeInclusao(LocalDate.now());
 
         System.out.println("Classe de Produto: ");
-        System.out.println("Codigo: "+ cp1.getCodigo());
-        System.out.println("Descrição: "+ cp1.getDescricao());
-        System.out.println("Data de inclusão: "+ cp1.getDataDeInclusao ());
+        System.out.println("Codigo: " + cp1.getCodigo());
+        System.out.println("Descrição: " + cp1.getDescricao());
+        System.out.println("Data de inclusão: " + cp1.getDataDeInclusao ());
 
         ClasseProduto cp2 = new ClasseProduto(2,"Bebidas",LocalDate.now());
         System.out.println("Classe de Produto: ");
-        System.out.println("Codigo: "+ cp2.getCodigo());
-        System.out.println("Descrição: "+ cp2.getDescricao());
-        System.out.println("Data de inclusão: "+ cp2.getDataDeInclusao ());
+        System.out.println("Codigo: " + cp2.getCodigo());
+        System.out.println("Descrição: " + cp2.getDescricao());
+        System.out.println("Data de inclusão: " + cp2.getDataDeInclusao ());
 
     }
 }
