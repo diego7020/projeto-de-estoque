e0eb0b05579da2eba1000a6a5912e987399cec1c
Diego Militao de Souza <diegomilitaosouza@gmail.com>
2024-10-08 20:42:34 -0400

    aula dia 8

--- src/visao/ClasseProdutoVisao.java
+++ src/visao/ClasseProdutoVisao.java
@@ -1,26 +1,26 @@
 package visao;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import dominio.ClasseProduto;
 
+import repositorio.ClasseProdutoRepo;
+
 public class ClasseProdutoVisao {
     public void Exibir(){
-        ClasseProduto cp1 = new ClasseProduto();
-        cp1.setCodigo(1);
-        cp1.setDescricao("Carnes");
-        cp1.setDataDeInclusao(LocalDate.now());
+        ClasseProdutoRepo repo = new ClasseProdutoRepo();
+        ArrayList<ClasseProduto> lista = repo.Browse();
+            for (ClasseProduto cp : lista) {
+            this.imprimir(cp);            
+            }
+        }
+        
 
+    private void imprimir(ClasseProduto alvo){
         System.out.println("Classe de Produto: ");
-        System.out.println("Codigo: " + cp1.getCodigo());
-        System.out.println("Descrição: " + cp1.getDescricao());
-        System.out.println("Data de inclusão: " + cp1.getDataDeInclusao ());
-
-        ClasseProduto cp2 = new ClasseProduto(2,"Bebidas",LocalDate.now());
-        System.out.println("Classe de Produto: ");
-        System.out.println("Codigo: " + cp2.getCodigo());
-        System.out.println("Descrição: " + cp2.getDescricao());
-        System.out.println("Data de inclusão: " + cp2.getDataDeInclusao ());
-
+        System.out.println("Codigo: "+ alvo.getCodigo());
+        System.out.println("Descrição: "+ alvo.getDescricao());
+        System.out.println("Data de inclusão: "+ alvo.getDataDeInclusao());
     }
+
 }
